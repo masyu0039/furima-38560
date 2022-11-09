54d08edfac196b06892ee2391e63f10dd6cdc77c
masyu0039 <114735314+masyu0039@users.noreply.github.com>
2022-11-09 20:55:30 +0900

    変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,11 @@ class ItemsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
   
     
-    before_action :set_item, only: [:show, :edit, :update, :destroy]
+    # before_action :set_item, only: [:show, :edit, :update, :destroy]
   
-    def index
-      @items = Item.includes(:user)
-    end
+    #def index
+      #@items = Item.includes(:user)
+    #end
   
     def new
       @item = Item.new
@@ -27,7 +27,7 @@ class ItemsController < ApplicationController
       params.require(:item).permit(:image, :name, :price, :description, :status_id, :prefecture_id, :category_id, :postage_id, :shipping_date_id).merge(user_id: current_user.id)
     end
   
-    def set_item
-      @item = Item.find(params[:id])
-    end
+    # def set_item
+      #@item = Item.find(params[:id])
+  #end
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,6 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :user
     validates :image
     validates :name
     validates :price, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
